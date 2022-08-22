65ff2a9387628d049e1838c8d591735c7038e5a5
EstebanUO <esteban.urrego@misena.edu.co>
2022-08-21 20:09:01 -0500

    Nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { Register } from "./Components/Register/Register";
+import { Register } from "./Components/Page/Register/Register";
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./Components/Page/Home/Home";
+import { Login } from "./Components/Page/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/register" element={<Register />}/>
+        <Route path="/login" element={<Login />}/>
+      </Routes>
     </div>
   );
 }
